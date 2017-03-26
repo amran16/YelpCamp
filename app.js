808b077e58474af4135bbb4928d78e3687adb141
amran16 <maryamg99@gmail.com>
2017-03-25 23:14:16 -0700

    added dotenv

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/yelp_camp_v10");
-//mongoose.connect("mongodb://yelp:abc@ds143000.mlab.com:43000/yelpcamp")
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://yelp:abc@ds143000.mlab.com:43000/yelpcamp")
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
